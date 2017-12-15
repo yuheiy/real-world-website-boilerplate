8028e32697eb6cdbbd2cd63921716325e4af38da
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2017-12-16 00:19:39 +0900

    format

--- task/renderHtml.js
+++ task/renderHtml.js
@@ -7,15 +7,15 @@ const { isProd, basePath, baseUrl, readFileAsync } = require('./util')
 const dataFileExts = ['.yml', '.yaml', '.json']
 
 const readFileData = async () => {
-  const filePaths = (await globby(dataFileExts.map(ext => `src/html/_data/*${ext}`), {
+  const filePaths = (await globby(dataFileExts.map((ext) => `src/html/_data/*${ext}`), {
     nodir: true,
   })).filter((filePath, i, arr) => {
     const { name } = path.parse(filePath)
-    const prevNames = arr.slice(0, i).map(item => path.parse(item).name)
+    const prevNames = arr.slice(0, i).map((item) => path.parse(item).name)
     return !prevNames.includes(name)
   })
   const fileData = await Promise.all(
-    filePaths.map(async filePath => ({
+    filePaths.map(async (filePath) => ({
       name: path.parse(filePath).name,
       data: yaml.safeLoad(await readFileAsync(filePath, 'utf8')),
     })),
@@ -30,8 +30,8 @@ const readFileData = async () => {
   return gatheredFileData
 }
 
-const readPageData = async pageFilePath => {
-  const [filePath] = await globby(dataFileExts.map(ext => pageFilePath.replace(/\.pug$/, ext)), {
+const readPageData = async (pageFilePath) => {
+  const [filePath] = await globby(dataFileExts.map((ext) => pageFilePath.replace(/\.pug$/, ext)), {
     nodir: true,
   })
   const fileData = filePath ? yaml.safeLoad(await readFileAsync(filePath)) : {}
@@ -51,12 +51,12 @@ const pugConfig = {
 }
 const baseLocals = {
   isProd,
-  absPath: pagePath => path.posix.join(`${basePath}/`, pagePath),
-  assetPath: pagePath => path.posix.join(`${basePath}/`, 'assets', pagePath),
-  absUrl: pagePath => `${baseUrl}${path.posix.join('/', pagePath)}`,
+  absPath: (pagePath) => path.posix.join(`${basePath}/`, pagePath),
+  assetPath: (pagePath) => path.posix.join(`${basePath}/`, 'assets', pagePath),
+  absUrl: (pagePath) => `${baseUrl}${path.posix.join('/', pagePath)}`,
 }
 
-const createTemplateConfig = async pageFilePath => {
+const createTemplateConfig = async (pageFilePath) => {
   return {
     ...pugConfig,
 
@@ -67,22 +67,22 @@ const createTemplateConfig = async pageFilePath => {
   }
 }
 
-const renderError = err => {
+const renderError = (err) => {
   return `<!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>${err.msg}</title>
-    </head>
-    <body>
-        <pre><code>${err.stack}</code></pre>
-    </body>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>${err.msg}</title>
+  </head>
+  <body>
+    <pre><code>${err.stack}</code></pre>
+  </body>
 </html>
 `
 }
 
-const renderHtml = async filePath => {
+const renderHtml = async (filePath) => {
   try {
     const config = await createTemplateConfig(filePath)
     return pug.renderFile(filePath, config)
