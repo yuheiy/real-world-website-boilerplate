5bbaf7dd701213f5571df95a649e64ca9c5cf780
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2018-03-03 21:41:16 +0900

    update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
+const LicenseInfoWebpackPlugin = require('license-info-webpack-plugin').default
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const LicenseBannerPlugin = require('license-banner-webpack-plugin')
 const { isProd, destAssetDir } = require('./task/util')
 
 module.exports = {
@@ -39,13 +39,29 @@ module.exports = {
     ],
   },
   plugins: [
-    new webpack.EnvironmentPlugin({
-      NODE_ENV: isProd ? 'production' : 'development',
-    }),
     new webpack.DefinePlugin({
       __DEV__: !isProd,
     }),
-    ...(isProd ? [new UglifyJsPlugin(), new LicenseBannerPlugin()] : []),
+    ...(isProd
+      ? [new LicenseInfoWebpackPlugin({ includeLicenseFile: false })]
+      : []),
   ],
-  devtool: !isProd && 'cheap-module-source-map',
+  mode: isProd ? 'production' : 'development',
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        // https://github.com/webpack/webpack/blob/5238159d211576bb313d99d487e415f3799af795/lib/WebpackOptionsDefaulter.js#L268-L270
+        cache: true,
+        parallel: true,
+        sourceMap: !isProd,
+
+        // for license-info-webpack-plugin
+        uglifyOptions: {
+          output: {
+            comments: /^\**!|@preserve|@license|@cc_on/,
+          },
+        },
+      }),
+    ],
+  },
 }
