29b7e4c7dbc5c1bb2bec3ea4eafb79aba3cbed93
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2018-12-31 06:35:59 +0900

    use em unit for media query also in JavaScript

--- src/js/mediaQuery.js
+++ src/js/mediaQuery.js
@@ -1,3 +1,14 @@
+const BROWSER_DEFAULT_FONT_SIZE = 16
+
+export const MEDIA_QUERY_SMALL = `(min-width: ${576 /
+  BROWSER_DEFAULT_FONT_SIZE}em)`
+export const MEDIA_QUERY_MEDIUM = `(min-width: ${768 /
+  BROWSER_DEFAULT_FONT_SIZE}em)`
+export const MEDIA_QUERY_LARGE = `(min-width: ${992 /
+  BROWSER_DEFAULT_FONT_SIZE}em)`
+export const MEDIA_QUERY_XLARGE = `(min-width: ${1200 /
+  BROWSER_DEFAULT_FONT_SIZE}em)`
+
 // extended version of
 // https://github.com/Polymer/pwa-helpers/blob/d0ff1d2cc5272a79d83fb130bea41e5ca5d25ea3/media-query.js
 export const installMediaQueryWatcher = (mediaQuery, layoutChangedCallback) => {
@@ -14,7 +25,3 @@ export const installMediaQueryWatcher = (mediaQuery, layoutChangedCallback) => {
   return uninstall
 }
 
-export const MEDIA_QUERY_SMALL = '(min-width: 576px)'
-export const MEDIA_QUERY_MEDIUM = '(min-width: 768px)'
-export const MEDIA_QUERY_LARGE = '(min-width: 992px)'
-export const MEDIA_QUERY_XLARGE = '(min-width: 1200px)'
